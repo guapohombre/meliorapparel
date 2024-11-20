dadb7c048ba7c840501205439ef91a1eae0ef81a
Tom Correll <thomasdcorrell@gmail.com>
2024-11-20 16:23:54 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const sections = document.querySelectorAll(".box");
+const throwaway = document.querySelector(".hero-throwaway-container");
+
+const throwawayObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("in-view-throwaway");
+        entry.target.classList.remove("not-in-view");
+      } else {
+        entry.target.classList.remove("in-view-throwaway");
+        entry.target.classList.add("not-in-view");
+      }
+    });
+  },
+  { threshold: [0.6] }
+);
+
+const sectionObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("in-view");
+        entry.target.classList.remove("not-in-view");
+      } else {
+        entry.target.classList.remove("in-view");
+        entry.target.classList.add("not-in-view");
+      }
+    });
+  },
+  { threshold: [0.1, 1] }
+);
+
+sections.forEach((section) => sectionObserver.observe(section));
+throwawayObserver.observe(throwaway);
